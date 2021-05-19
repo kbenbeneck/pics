263c9c224de22725cdb6b2744a9a4187f102743b
KBENBENECK <kbenbeneck@gmail.com>
2021-05-19 11:18:34 -0400

    Add async await function. include search term params and auth in get request.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term)
-        //take search term and fetch
+    async onSearchSubmit(term) {
+        const response = await axios
+        .get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID OY78ffadHp7Lch-DsIDgciVbaFKID3q49RpAdvq0ux4'
+            }
+        });
+        console.log(response.data.results)
     }
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
